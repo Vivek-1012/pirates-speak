5149d639fb26e6734d5334378403022db20c382a
Vivek-1012 <110529479+Vivek-1012@users.noreply.github.com>
2022-09-07 23:58:27 +0530

    doc
    alert added

--- main.js
+++ main.js
@@ -8,6 +8,13 @@ function getTranslationURL(input) {
      return serverURL + "?" + "text=" + input
 }
 
+function errorHandler(error) {
+    console.log("error occured", error);
+    alert("something wrong with server! try again after some time")
+}
+
+
+
 function clickHandler() {
     // outputDiv.innerText = "ajsjsjsjsjsjsj " + inputTxt.value;
    var inputText = inputTxt.value;
@@ -19,6 +26,8 @@ function clickHandler() {
     var translatedText = json.contents.translated;
     outputDiv.innerText = translatedText;
    })
+
+   .catch(errorHandler)
     
    
 };
